434ff4f4be9e99bcccb28b9f0cfdd8d710f90158
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2023-01-07 21:16:01 +0900

    [Django : tutorial]  title : 모델 만들기
    모델 설계
    모델 생성 : 모델 정의, 데이터베이스 설정, 앱 등록하기, 모델 수정
    장고 쉘에서 테스트 : 데이터 저장, 데이터 표시 형식 변경 , 데이터 검색, 수정, 저장
    Admin 사이트 : 관리자 등록하기, Admin 사이트 접속, Admin 사이트에 등록, Admin 사이트 커스터마이징

--- DjangoTutorial/bbs/models.py
+++ DjangoTutorial/bbs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title      = models.CharField('제목', max_length=126, null=False)
+    content    = models.TextField('내용', null=False)
+    author     = models.CharField('작성자', max_length=16, null=False)
+    created_at = models.DateTimeField('작성일', auto_now_add=True)
+    created_at.editable = True                                     # created의 editable 속성에 True를 설정했습니다.
+
+    def __str__(self):
+        return '[{}] {}'.format(self.id, self.title)
