219b9b67d31375f47989d57e7c7f41661a0e3f02
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2023-01-09 15:53:33 +0900

    [Django : tutorial]  title : 뷰 만들기
    뷰 설계
    뷰 생성 : 화면 핸들러 정의하기, 액션 핸들러 정의하기 ,CBV로 변환, 데이터 검색, CSRF verification

--- DjangoTutorial/bbs/views.py
+++ DjangoTutorial/bbs/views.py
@@ -1,6 +1,92 @@
-# bbs/views.py
+from django.http import HttpResponse, Http404
+from django.views.generic import TemplateView
+from bbs.models import Article
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
-from django.http import HttpResponse
+# 게시글 목록
+class ArticleListView(TemplateView):
+    template_name = 'base.html'
+    queryset = Article.objects.all()         # 모든 게시글
 
-def hello(request):                     # 핸들러 선언. 언제나 첫번째 인자는 request 객체입니다.
-    return HttpResponse('Hello world.') # 핸들러의 반환값. HttpResponse 함수를 통해 문자열을 반환합니다.
+    def get(self, request, *args, **kwargs):
+
+        # 템플릿에 전달할 데이터
+        ctx = {
+            'view': self.__class__.__name__, # 클래스의 이름
+            'data': self.queryset            # 검색 결과
+        }
+        return self.render_to_response(ctx)
+
+# 게시글 상세
+class ArticleDetailView(TemplateView):
+    template_name = 'base.html'
+    queryset = Article.objects.all()
+    pk_url_kwargs = 'article_id'                 # 검색데이터의 primary key를 전달받을 이름
+
+    def get_object(self, queryset=None):
+        queryset = queryset or self.queryset     # queryset 파라미터 초기화
+        pk = self.kwargs.get(self.pk_url_kwargs) # pk는 모델에서 정의된 pk값, 즉 모델의 id
+        return queryset.filter(pk=pk).first()    # pk로 검색된 데이터가 있다면 그 중 첫번째 데이터 없다면 None 반환
+
+    def get(self, request, *args, **kwargs):
+        article = self.get_object()
+        if not article:
+            raise Http404('invalid article_id')  # 검색된 데이터가 없다면 에러 발생
+
+        ctx = {
+            'view': self.__class__.__name__,
+            'data': article
+        }
+        return self.render_to_response(ctx)
+
+# 게시글 추가, 수정
+@method_decorator(csrf_exempt, name='dispatch')   # 모든 핸들러 예외 처리
+class ArticleCreateUpdateView(TemplateView):
+    template_name = 'base.html'
+    queryset = Article.objects.all()
+    pk_url_kwargs = 'article_id'
+    
+    def get_object(self, queryset=None):
+        queryset = queryset or self.queryset
+        pk = self.kwargs.get(self.pk_url_kwargs)
+        article = queryset.filter(pk=pk).first()
+        
+        if pk and not article:                    # 검색결과가 없으면 곧바로 에러 발생
+            raise Http404('invalid pk')
+        return article
+
+    def get(self, request, *args, **kwargs):
+        article = self.get_object()
+
+        ctx = {
+            'view': self.__class__.__name__,
+            'data': article
+        }
+        return self.render_to_response(ctx)
+
+    def post(self, request, *args, **kwargs):
+        action = request.POST.get('action')
+        post_data = {key: request.POST.get(key) for key in ('title', 'content', 'author')}
+        for key in post_data:
+            if not post_data[key]:
+                raise Http404('no data for {}'.format(key))
+
+        if action == 'create':
+            article = Article.objects.create(title=title, content=content, author=author)
+        elif action == 'update':
+            article = self.get_object()
+            for key, value in post_data.items():
+                setattr(article, key, value)
+            article.save()
+        else:
+            raise Http404('invalid action')
+
+        ctx = {
+            'view': self.__class__.__name__,
+            'data': article
+        }
+        return self.render_to_response(ctx)
+        
+def hello(request, to):
+    return HttpResponse('Hello {}.'.format(to))
--- DjangoTutorial/djangotutorial/urls.py
+++ DjangoTutorial/djangotutorial/urls.py
@@ -3,9 +3,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from bbs.views import hello          # 작성한 핸들러를 사용할 수 있게 가져옵니다.
+# 작성한 핸들러를 사용할 수 있게 가져옵니다.
+from bbs.views import hello, ArticleListView, ArticleCreateUpdateView, ArticleDetailView          
 
 urlpatterns = [
-    path('hello/', hello),           # 'hello/'라는 경로로 접근했을 때 hello 핸들러가 호출되도록 추가합니다.
+    path('hello/<to>', hello),           # 'hello/'라는 경로로 접근했을 때 hello 핸들러가 호출되도록 추가합니다.
     path('admin/', admin.site.urls),
+
+    path('article/', ArticleListView.as_view()),
+    path('article/create/', ArticleCreateUpdateView.as_view()),
+    path('article/<article_id>/', ArticleDetailView.as_view()),
+    path('article/<article_id>/update/', ArticleCreateUpdateView.as_view()),
 ]
