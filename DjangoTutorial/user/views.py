5a0ec9f6157154ce16454e7387cc52aa2d25d886
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2023-01-21 14:33:58 +0900

    [Django : tutorial]  title : 소셜 로그인 (1)
    네이버 소셜로그인 [네아로] 앱 등록
    네이버 소셜로그인 템플릿 생성
    네이버 로그인 javascript 구현
    네이버 로그인 callback 뷰 구현

--- DjangoTutorial/user/views.py
+++ DjangoTutorial/user/views.py
@@ -12,6 +12,12 @@ from user.mixins import VerifyEmailMixin
 
 from django.views.generic import CreateView, FormView
 
+# 소셜 로그인
+from django.conf import settings
+from django.views.generic.base import TemplateView, View
+from django.middleware.csrf import _compare_salted_tokens
+from user.oauth.providers.naver import NaverLoginMixin
+
 
 class UserRegistrationView(VerifyEmailMixin, CreateView):
     template_name = 'registration_form.html'
@@ -72,3 +78,36 @@ class ResendVerifyEmailView(VerifyEmailMixin, FormView):
         else:
             self.send_verification_email(user)
         return super().form_valid(form)
+
+# user/views.py
+
+
+# 생략
+
+class SocialLoginCallbackView(NaverLoginMixin, View):
+
+    success_url = settings.LOGIN_REDIRECT_URL
+    failure_url = settings.LOGIN_URL
+    required_profiles = ['email', 'name']
+    model = get_user_model()
+
+    def get(self, request, *args, **kwargs):
+
+        provider = kwargs.get('provider')
+
+        if provider == 'naver': # 프로바이더가 naver 일 경우
+            csrf_token = request.GET.get('state')
+            code = request.GET.get('code')
+            if not _compare_salted_tokens(csrf_token, request.COOKIES.get('csrftoken')): # state(csrf_token)이 잘못된 경우
+                messages.error(request, '잘못된 경로로 로그인하셨습니다.', extra_tags='danger')
+                return HttpResponseRedirect(self.failure_url)
+            is_success, error = self.login_with_naver(csrf_token, code)
+            if not is_success: # 로그인 실패할 경우
+                messages.error(request, error, extra_tags='danger')
+            return HttpResponseRedirect(self.success_url if is_success else self.failure_url)
+
+        return HttpResponseRedirect(self.failure_url)
+
+    def set_session(self, **kwargs):
+        for key, value in kwargs.items():
+            self.request.session[key] = value
